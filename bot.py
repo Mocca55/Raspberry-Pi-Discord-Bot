d9ba90d9b8427f7f98a9a61a5e60ca3666e4988c
Mocca55 <65739996+Mocca55@users.noreply.github.com>
2021-02-14 19:46:57 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,153 @@
+# Import der Bibliotheken
+import discord
+import time
+from discord.ext import commands
+import datetime
+import os
+from time import sleep
+
+
+#Startdaten anzeigen und per Print ausgeben und in Logdatei schreiben
+now = datetime.datetime.now()
+print("Gestartet")
+print(str(now))
+datei = open('bot-log.txt','a')
+datei.write("\r\n" + str(now) + "     Bot gestartet")
+datei.close()
+
+
+# Discord Token
+TOKEN = 'Token hier einfügen'
+
+#Setzt den Command Prefix
+client = commands.Bot(command_prefix='-')
+
+# Start Information mit Print Ausgabe
+@client.event
+async def on_ready():
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
+
+
+@client.command() 
+async def netflix(ctx):
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Netflix'))
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Wechsel auf -> schaut Netflix")
+    datei.write("\r\n" + str(now) + "     Wechsel auf -> schaut Netflix")
+    datei.close()
+
+@client.command() 
+async def rl(ctx):
+    await client.change_presence(activity=discord.Game(name="Rocket League"))
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Wechsel auf -> spiel Rocket League")
+    datei.write("\r\n" + str(now) + "     Wechsel auf -> spiel Rocket League")
+    datei.close()
+    
+
+@client.command() 
+async def mc(ctx):
+    await client.change_presence(activity=discord.Game(name="Minecraft-Server"))
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Wechsel auf -> spielt Minecraft-Server")
+    datei.write("\r\n" + str(now) + "     Wechsel auf -> spielt Minecraft-Server")
+    datei.close()
+    
+@client.command() 
+async def bf(ctx):
+    await client.change_presence(activity=discord.Game(name="Star Wars Battlefront II"))
+    print(str(now) + "     Game BF2")
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Wechsel auf -> spielt BF2")
+    datei.write("\r\n" + str(now) + "     Wechsel auf -> spielt BF2")
+    datei.close()
+
+@client.command() 
+async def friday(ctx):
+    await ctx.send('Friday da möchte jemand spielen..... GameON')
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Command -> friday")
+    datei.write("\r\n" + str(now) + "     Command -> friday")
+    datei.close()
+@client.command() 
+async def passstift(ctx):
+    await ctx.send('Adrian da möchte jemand spielen..... GameON')
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Command -> Passstift")
+    datei.write("\r\n" + str(now) + "     Command -> Passstift")
+    datei.close()
+
+@client.command() 
+async def mocca55(ctx):
+    await ctx.send('Michael da möchte jemand spielen..... GameON')
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Command -> Mocca55")
+    datei.write("\r\n" + str(now) + "     Command -> Mocca55")
+    datei.close()
+    
+@client.command() 
+async def minecraft(ctx):
+    await ctx.send('GameON Minecraft Server XX.XX.XXX.XXX')
+    now = datetime.datetime.now()
+    datei = open('bot-log.txt','a')
+    print(str(now) + "     Mincraft Server Abfrage")
+    datei.write("\r\n" + str(now) + "     Mincraft Server Abfrage")
+    datei.close()
+
+#@client.command()
+#@commands.is_owner()
+#async def shutdown(ctx):
+ #   await ctx.bot.logout()
+  #  now = datetime.datetime.now()
+   # datei = open('bot-log.txt','a')
+    #datei.write("\r\n" + str(now) + "     Bot beendet")
+    #datei.close()
+    
+import os
+@client.command() 
+async def temp(ctx):
+        tempData = "/sys/class/thermal/thermal_zone0/temp"
+        dateilesen = open(tempData, "r")
+        temperatur = dateilesen.readline(2)
+        dateilesen.close()
+        now = datetime.datetime.now()
+        datei = open('bot-log.txt','a')
+        print(str(now) + "     Deine CPU hat " + temperatur + " Grad")
+        datei.write("\r\n" + str(now) + "     Deine CPU hat " + temperatur + " Grad")
+        await ctx.send("Meine CPU Temperatur ist gerade " +  temperatur +  " Grad.")
+        datei.close()
+
+
+@client.command() 
+async def serverrestart(ctx):
+        now = datetime.datetime.now()
+        datei = open('bot-log.txt','a')
+        print(str(now) + "     Minecraft Server Restart")
+        datei.write("\r\n" + str(now) + "     Minecraft Server Restart")
+        await ctx.send("Einen moment der Minecraft Server wird jetzt neu gestartet.")
+        stop = os.system("~/minecraft/stop.sh")
+        start = os.system("~/minecraft/start.sh")
+        sleep (47)
+        await ctx.send("Minecraft Server ist neu gestartet und bereit.")
+        datei.close()
+        
+@client.command() 
+async def serverbackup(ctx):
+        now = datetime.datetime.now()
+        datei = open('bot-log.txt','a')
+        print(str(now) + "     Minecraft Server Backup auf USB-Stick")
+        datei.write("\r\n" + str(now) + "     Minecraft Server Backup auf USB-Stick")
+        await ctx.send("Einen moment Backup wird jetzt erstellt.")
+        backup = os.system("cp /home/pi/minecraft /media/pi/USB-Backup -r -u -v")
+        await ctx.send("Server Backup wurde auf dem USB-Stick erstellt.")
+        datei.close()
+
+client.run(TOKEN)
